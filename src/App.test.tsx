e840f70f5edd2031a3003e029539075b73cd9646
Willian <wyllmh5@gmail.com>
2021-04-27 11:30:35 -0600

    Added tests for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,82 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { mount, ReactWrapper } from 'enzyme';
+import { findByTestAttr } from './utils/test';
+import { AppStateContext } from './context/AppContext';
+import { initialState } from './context/AppContext/initialState';
+import { QuestionData, State } from './interfaces';
 import App from './App';
-import { AppProvider } from './context/AppContext';
 
-test('renders learn react link', () => {
-  render(
-    <AppProvider>
+const dispatch = jest.fn();
+const fetchQuestionsList = jest.fn();
+const questionsList: QuestionData[] = [
+  {
+    category: 'Category 1',
+    difficulty: 'hard',
+    type: 'boolean',
+    question: 'Question 1?',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Category 2',
+    difficulty: 'hard',
+    type: 'boolean',
+    question: 'Question 2?',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+];
+
+const state = { ...initialState, questionsList };
+const setup = (props?: State) => {
+  const setupState = { ...state, ...props };
+  return mount(
+    <AppStateContext.Provider value={{ state: setupState, dispatch }}>
       <App />
-    </AppProvider>
+    </AppStateContext.Provider>
   );
-  /* const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument(); */
+};
+
+describe('App component', () => {
+  let wrapper: ReactWrapper;
+
+  beforeEach(() => {
+    wrapper = setup();
+  });
+
+  test('Should render properly', () => {
+    const mainPageComponent = findByTestAttr(wrapper, 'mainPage');
+    expect(mainPageComponent).toHaveLength(1);
+  });
+
+  test('Should call disptach when component is loaded', () => {
+    expect(dispatch.mock.calls.length).toEqual(1);
+    console.log('FETCH: ', fetchQuestionsList.mock.calls.length);
+  });
+
+  test('Should only render QuestionCard component when showQuestionCard = true', () => {
+    const wrapper = setup({
+      ...state,
+      showQuestionCard: true,
+      showHomePage: false,
+      showResultsCard: false,
+    });
+
+    expect(wrapper.find('HomePage').length).toEqual(0);
+    expect(wrapper.find('QuestionCard').length).toEqual(1);
+    expect(wrapper.find('ResultsCard').length).toEqual(0);
+  });
+
+  test('Should only render ResultsCard component when showResultsCard = true', () => {
+    const wrapper = setup({
+      ...state,
+      showQuestionCard: false,
+      showHomePage: false,
+      showResultsCard: true,
+    });
+
+    expect(wrapper.find('HomePage').length).toEqual(0);
+    expect(wrapper.find('QuestionCard').length).toEqual(0);
+    expect(wrapper.find('ResultsCard').length).toEqual(1);
+  });
 });
